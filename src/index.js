03534824c3011afc7708b63a4080b527f2932214
Milk-Cool <milan-pl@mail.ru>
2024-05-17 19:47:55 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/iconbutton/icon-button.js';
+import '@material/web/icon/icon.js';
+import '@material/web/progress/linear-progress.js'
